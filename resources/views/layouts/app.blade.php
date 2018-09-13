cf60144272fdeb90c8b2e0ee712709616bb0b58c
void-sister <el.varvara.el@gmail.com>
2018-09-13 05:29:12 +0300

    Adjust html template + add simple routes

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta name="description" content="">
+      <meta name="author" content="">
+      <title>@yield('title') | E-Shopper</title>
+      <link href="{{ URL::asset('css/bootstrap.min.css') }}" rel="stylesheet">
+      <link href="{{ URL::asset('css/font-awesome.min.css') }}" rel="stylesheet">
+      <link href="{{ URL::asset('css/prettyPhoto.css') }}" rel="stylesheet">
+      <link href="{{ URL::asset('css/price-range.css') }}" rel="stylesheet">
+      <link href="{{ URL::asset('css/animate.css') }}" rel="stylesheet">
+      <link href="{{ URL::asset('css/main.css') }}" rel="stylesheet">
+    	<link href="{{ URL::asset('css/responsive.css') }}" rel="stylesheet">
+      <!--[if lt IE 9]>
+      <script src="{{ URL::asset('js/html5shiv.js') }}"></script>
+      <script src="{{ URL::asset('js/respond.min.js') }}"></script>
+      <![endif]-->
+      <link rel="shortcut icon" href="images/ico/favicon.ico">
+      <link rel="apple-touch-icon-precomposed" sizes="144x144" href="{{ URL::asset('images/ico/apple-touch-icon-144-precomposed.png') }}">
+      <link rel="apple-touch-icon-precomposed" sizes="114x114" href="{{ URL::asset('images/ico/apple-touch-icon-114-precomposed.png') }}">
+      <link rel="apple-touch-icon-precomposed" sizes="72x72" href="{{ URL::asset('images/ico/apple-touch-icon-72-precomposed.png') }}">
+      <link rel="apple-touch-icon-precomposed" href="{{ URL::asset('images/ico/apple-touch-icon-57-precomposed.png') }}">
+    </head>
+    <body>
+      @include('partials.header')
+
+      <div class="container">
+          @yield('content')
+      </div>
+
+      @include('partials.footer')
+
+      @section('js_links')
+      <script src="{{ URL::asset('js/jquery.js') }}"></script>
+  	  <script src="{{ URL::asset('js/bootstrap.min.js') }}"></script>
+      <script src="{{ URL::asset('js/jquery.scrollUp.min.js') }}"></script>
+      <script src="{{ URL::asset('js/jquery.prettyPhoto.js') }}"></script>
+      <script src="{{ URL::asset('js/main.js') }} "></script>
+      @show
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,31 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+
+Route::get('/blog-single', function () {
+    return view('blog-single');
+});
+Route::get('/blog', function () {
+    return view('blog');
+});
+Route::get('/cart', function () {
+    return view('cart');
+});
+Route::get('/checkout', function () {
+    return view('checkout');
+});
+Route::get('/contact-us', function () {
+    return view('contact-us');
+});
+Route::get('/login', function () {
+    return view('login');
+});
+Route::get('/product-details', function () {
+    return view('product-details');
+});
+Route::get('/shop', function () {
+    return view('shop');
 });
